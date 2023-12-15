81108bdb2c1cb7d41f12f96282242787bcb394c0
Laura Robinson <laura3918@hotmail.co.uk>
2023-12-15 22:34:40 +0000

    feat: useEffect now fetches a toDo

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,40 @@
-// make an api call using useEffect
+import { useEffect, useState } from "react";
+
+interface ToDo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 export const APICall = () => {
+  const [toDo, setToDo] = useState<ToDo>();
 
-    // Your code here!
+  const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+  useEffect(() => {
+    const fetchData = async (apiEndPoint: string) => {
+      const response = await fetch(apiEndPoint);
+      try {
+        const json = await response.json();
+        console.log(json);
+        setToDo(json as ToDo);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData(jsonTypicode);
+    console.log(toDo);
+  }, []);
+
+  return (
+    <>
+      <h2>useEffect</h2>
+      <p>UserID: {toDo?.userId}</p>
+      <p>ID: {toDo?.id}</p>
+      <p>Title: {toDo?.title}</p>
+      <p>Completed: {toDo?.completed.toString()}</p>
+    </>
+  );
+};
